8c233c28773971ef31ea40f8e51a8b7941e283b9
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-09 17:13:57 -0300

    added final code

--- src/components/accordion/data.js
+++ src/components/accordion/data.js
@@ -1,13 +1,13 @@
 const data = [
   {
     id  : '1',
-    question: "What are accordion components?",
+    question: "What are accordion components ?",
     answer:
       "Accordion components are user interface elements used for organizing and presenting content in a collapsible manner. They typically consist of a header, content, and an expand/collapse action.",
   },
   {
     id  : '2',
-    question: "What are they used for?",
+    question: "What are they used for ?",
     answer:
       "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a structured and user-friendly interface for presenting information or options.",
   },
