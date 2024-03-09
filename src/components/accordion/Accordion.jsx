bf6c1c86f703b83aae8968c25bc48976e479f821
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-09 16:32:01 -0300

    added functions and styles for the accordion

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
-import data from "./data"
+import data from "./data";
+import './styles.css';
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {
+     setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
 
   return (
     <div className="wrapper">
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item">
-              <div className="title">
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
