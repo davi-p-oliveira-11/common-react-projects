717b5e9c02bbe46bd3f79045ae527f66af00a80e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-11 15:40:30 -0300

    added image slider and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
-import './App.css'
-import Accordion from './components/accordion/Accordion'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
+import "./App.css";
+import Accordion from "./components/accordion/Accordion";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -14,10 +14,16 @@ function App() {
       {/* <RandomColor /> */}
 
       {/* Star Rating Component */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"10"}
+        page={"1"}
+      />
     </div>
-  )
-  
+  );
 }
 
-export default App
+export default App;
--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { FaStar } from "react-icons/fa";
-import "./styles.css"
+import "./styles.css";
 
-
-export default function StarRating({noOfStars = 5}) {
-
+export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
@@ -20,23 +18,22 @@ export default function StarRating({noOfStars = 5}) {
     setHover(rating);
   }
 
-  return <div className="star-rating">
-     {
-      [...Array(noOfStars)].map((_,index) => {
-        index += 1
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
 
-        return <FaStar
-          key={index}
-          className={index <= (hover || rating) ? 'active' : 'inactive'}
-          onClick={() => handleClick(index)}
-          onMouseMove={() => handleMouseEnter(index)}
-          onMouseLeave={() => handleMouseLeave(index)}
-          size={40}
-        />
-      })
-     }
-   
-
-  </div>
-
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
 }
